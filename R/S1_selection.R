1262f72040b504a8da8e8240eb6ebf4e1c34ba83
jendelman <endelman@wisc.edu>
2022-10-27 14:27:30 -0500

    v0.26

--- R/S1_selection.R
+++ R/S1_selection.R
@@ -2,117 +2,111 @@
 #' 
 #' Signatures of selection in S1 populations
 #' 
-#' The null hypothesis is no selection, in which case the expected frequency of genotypes is (AA = 1/4, AB = 1/2, BB = 1/4). Two alternate hypotheses, or "modes", are tested for gametic selection: 1.selection in one sex, 2.selection in both sexes. Two modes of zygotic selection are also tested: 1.selection against one homozygote, 2.selection against both homozygotes. In addition to these 4 models with pure gametic or zygotic selection, 4 models with both forms of selection are considered (2 gametic modes x 2 zygotic modes = 4 models). The selection coefficients and -log10(p) value are returned for the model with the lowest AIC. The selection coefficients represent the sum of the absolute differences between the observed and expected frequencies. Positive values correspond to selection against A or AA, negative values for selection against B or BB. For zygotic mode 2, positive values represent selection against homozygotes, while negative values indicate selection against heterozygotes.  P-values are computed based on the likelihood ratio test; in other words, the change in deviance is assumed to be chi-squared distributed under the null hypothesis.
+#' Genotypes must be coded based on the S1 parental haplotypes, not markers.
+#'
+#' The null hypothesis is no selection, in which case the expected frequency of genotypes is (AA = 1/4, AB = 1/2, BB = 1/4). Two alternate hypotheses are tested for gametic selection: 1.selection in one sex, 2.selection in both sexes. Two models of zygotic selection are also tested: 1.selection against one homozygote, 2.selection against both homozygotes. The selection coefficient equals the sum of the absolute differences between the observed and expected frequencies. Positive values correspond to selection against A or AA, negative values for selection against B or BB. For zygotic2, positive (negative) values represent selection against (for) homozygotes. 
 #' 
-#' @param x Vector of progeny counts for the three possible genotypes (AA, AB, BB)
-#' @param pure.modes When FALSE, combined gametic and selection modes are tested
+#' P-values are computed based on the likelihood ratio test; in other words, the change in deviance is assumed to be chi-squared distributed under the null hypothesis.
 #' 
-#' @return List containing
+#' @param data data frame with columns: marker, chrom, position, AA, AB, BB. Columns 4-6 have count data.
+#' @param alpha significance level
+#' 
+#' @return list with "plot" and "table" of results:
 #' \describe{
-#' \item{s}{selection coefficients}
-#' \item{model}{the gametic and zygotic selection modes (NA,1,2) for the best model}
+#' \item{model}{name of best model}
+#' \item{s}{selection coefficient}
 #' \item{score}{-log10(p) value}
 #' }
 #' 
 #' @importFrom stats optimize pchisq
-#' @importFrom utils capture.output
-#' @import optimx
+#' @import ggplot2
 #' @export
 #' 
 
-S1_selection <- function(x,pure.modes=TRUE) {
+S1_selection <- function(data,alpha=0.05) {
   
-  freq <- function(s,mode) {
-
-    gamete.mode <- mode[1]
-    zygote.mode <- mode[2]
-    
-    k <- which(is.na(mode))
-    if (length(k) > 0) {
-      #one selection coefficient
-      if (k==1) {
-        sg <- 0
-        sz <- s
-        gamete.mode <- 1
-      } else {
-        sz <- 0
-        sg <- s
-        zygote.mode <- 1
-      }
-    } else {
-      sg <- s[1]
-      sz <- s[2]
-    }
-    
-    if (gamete.mode==1) {
-      p1 <- 1/2 - sg/2
-      p2 <- 1/2
-    } else {
-      p1 <- 1/2 - sg/2
-      p2 <- 1/2 - sg/2
-    }
-    q1 <- 1 - p1
-    q2 <- 1 - p2
-    
-    f <- function(s) {
-      ifelse(s > 0,s/2,s/4)
-    }
-    
-    #Zygote frequencies
-    P <- p1*p2
-    H <- p1*q2 + q1*p2
-    Q <- q1*q2
-    
-    if (zygote.mode==1) {
-      P <- P - f(sz)
-      H <- H + abs(sz)/4
-      Q <- Q - f(-sz)
-    } else {
-      P <- P - sz/4
-      H <- H + sz/2
-      Q <- Q - sz/4
-    }
-    return(c(P,H,Q))
+  get_x <- function(map) {
+    #takes a map with chrom and position and returns x axis values for plotting multiple chromosomes
+    a <- tapply(map[,2],map[,1],max)
+    n <- length(a)
+    m <- tapply(map[,2],map[,1],length)
+    b <- c(0,apply(array(1:(n-1)),1,function(k){sum(a[1:k])}))
+    x <- map[,2] + rep(b,times=m)
+    return(x)
   }
   
-  LL <- function(s,x,mode) {
-    p <- freq(s,mode)
-    return(sum(x*log(p)))
+  f <- function(s) {
+    ifelse(s > 0,s/2,s/6)
   }
   
-  cases <- rbind(c(1,NA),c(2,NA),c(NA,1),c(NA,2))
-  colnames(cases) <- c("gamete","zygote")
-  n.param <- rep(1,4)
-  if (!pure.modes) {
-    n.param <- c(n.param,rep(2,4))
-    tmp <- expand.grid(1:2,1:2)
-    colnames(tmp) <- colnames(cases)
-    cases <- rbind(cases,tmp)
+  freq <- function(s,model) {
+    #return zygote frequencies 
+    switch(model,
+           gametic1=1/4*c(1-s,2,1+s),
+           gametic2=1/4*c((1-s)^2,1-s^2,(1+s)^2),
+           zygotic1=c(1/4-f(s),1/2+abs(s)/3,1/4-f(-s)),
+           zygotic2=1/4*c(1-s,2*(1+s),1-s))
   }
-  cases <- split(cases,1:nrow(cases))
-  suppressWarnings(ans <- lapply(cases,function(mode){
-    mode <- as.integer(mode)
-    if (any(is.na(mode))) {
-      capture.output(ans <- optimx(par=0,fn=LL,gr=NULL,lower=-1,upper=1,
-             x=x,mode=mode,control=list(maximize=TRUE),method=c("L-BFGS-B","nlminb","Rcgmin","Rvmmin")))
-      k <- which.max(ans$value)
-      list(ans[k,1],ans[k,2])
+  
+  f.LL <- function(s,N,model) {
+    p <- freq(s,model)
+    return(sum(N*log(p)))
+  }
+  
+  x <- data[,4:6]
+  map <- data[,1:3]
+  colnames(map) <- c("marker","chrom","position")
+  
+  models <- c("gametic1","gametic2","zygotic1","zygotic2")
+  s <- NULL
+  LL <- NULL
+  for (i in 1:4) {
+    if (i==3) {
+      limits <- c(-0.5+1e-3,0.5-1e-3)
     } else {
-      capture.output(ans <- optimx(par=c(0,0),fn=LL,gr=NULL,lower=c(-1,-1),upper=c(1,1),
-             x=x,mode=mode,control=list(maximize=TRUE),method=c("L-BFGS-B","nlminb","Rcgmin","Rvmmin")))
-      k <- which.max(ans$value)
-      list(as.numeric(ans[k,1:2]),ans[k,3])
+      limits <- c(-1+1e-3,1-1e-3)
     }
-  }))
+    y <- t(apply(x,1,function(N){
+            as.numeric(optimize(f.LL,interval=limits,N=N,model=models[i],maximum=TRUE))}))
+    s <- cbind(s,y[,1])
+    LL <- cbind(LL,y[,2])
+  }
+  ix <- apply(LL,1,which.max)
   
-  AIC <- 2*n.param - 2*sapply(ans,"[[",2)
+  m <- nrow(x)
+  LL1 <- LL[cbind(1:m,ix)]
+  LL0 <- apply(x,1,function(N){f.LL(s=0,N=N,model="gametic1")})
+  score <- -log10(pchisq(q=2*(LL1-LL0),df=1,lower.tail=F))
+  output <- data.frame(map,model=models[ix],s=round(s[cbind(1:m,ix)],3),score=round(score,3))
   
-  LL0 <- LL(s=c(0,0),x=x,mode=c(1,1))
-  k <- which.min(AIC)
-  LL1 <- as.numeric(ans[[k]][2])
-  soln <- ans[[k]][[1]]
-  score <- -log10(pchisq(q=2*(LL1-LL0),df=length(soln),lower.tail=F))
-  model <- as.numeric(cases[[k]])
-  names(model) <- c("gametic","zygotic")
-  return(list(s=soln,model=model,score=score))
+  threshold <- -log10(alpha/m)
+  plotme <- data.frame(output,color=ifelse(score > threshold,output$model,"NS"))
+  chrom <- unique(map$chrom)
+  nchr <- length(chrom)
+  plotme$model <- factor(plotme$model,levels=c(models,"NS"))
+  colours <- c("#66C2A5","#FC8D62","#8DA0CB","#E78AC3","gray60")
+  names(colours) <- levels(plotme$model)
+  
+  if (nchr==1) {
+    p <- ggplot(data=plotme,aes(x=.data$position,y=.data$score,colour=.data$color)) + 
+      geom_point() + 
+      scale_colour_manual(name="Model",values=colours) + theme_bw() + 
+      ylab(expression(paste("-log"[10],"(p)"))) + theme(text = element_text(size=13)) + 
+      xlab("Position") + geom_hline(yintercept=threshold,linetype=2,col="gray30") 
+  } else {
+    x <- get_x(map[,2:3])
+    break1 <- (tapply(x,map$chrom,max) + tapply(x,map$chrom,min))/2
+    break2 <- (tapply(x,map$chrom,max)[-nchr] + tapply(x,map$chrom,min)[-1])/2
+    plotme$x <- x
+    p <- ggplot(data=plotme,aes(x=.data$x,y=.data$score,colour=.data$color)) +
+      geom_point() + 
+      scale_colour_manual(name="Model",values=colours) + theme_bw() + 
+      scale_x_continuous(name="Chromosome",breaks=break1,labels=chrom,
+                         minor_breaks = break2) +
+      ylab(expression(paste("-log"[10],"(p)"))) + 
+      theme(text = element_text(size=13), 
+            panel.grid.major.x = element_blank()) +
+      geom_hline(yintercept=threshold,linetype=2,col="gray30")
+  }
+  return(list(table=output,plot=p))
 }
