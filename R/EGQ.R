b3867fb72c1d50eb9dd29e278a1fbe993f88f5d3
jendelman <endelman@wisc.edu>
2023-02-12 20:58:20 -0600

    added prior to EGQ

--- R/EGQ.R
+++ R/EGQ.R
@@ -7,27 +7,28 @@
 #' @param depth read count
 #' @param error allelic error
 #' @param ploidy ploidy
+#' @param prior numeric vector of length ploidy + 1
 #' 
 #' @return numeric scalar
 #' @export
 #' @references Matias et al. (2019) Plant Genome 12:190002. https://doi.org/10.3835/plantgenome2019.01.0002
 
-EGQ <- function(depth,error,ploidy) {
+EGQ <- function(depth,error,ploidy,prior) {
   
-  Likelihood <- function(k,N,error,ploidy){
+  posterior <- function(k,N,error,ploidy,prior){
     p <- seq(0,1,by=1/ploidy)
     q <- 1-p
-    z <- dbinom(x=k,size=N,prob=p*(1-error)+q*error)
+    z <- dbinom(x=k,size=N,prob=p*(1-error)+q*error)*prior
     names(z) <- 0:ploidy
     return(z/sum(z))
   }
   
-  f1 <- function(N,error,ploidy) {
+  f1 <- function(N,error,ploidy,prior) {
     k <- seq(0,N,by=1)
-    p <- apply(array(k),1,Likelihood,N=N,error=error,ploidy=ploidy)
+    p <- apply(array(k),1,posterior,N=N,error=error,ploidy=ploidy,prior=prior)
     geno.mode <- apply(p,2,which.max)-1
     prob.error <- sum(dbinom(k,N,prob=0.5)[which(geno.mode!=ploidy/2)])
     return(-10*log10(prob.error))
   }
-  min(apply(array(depth:(depth+10)),1,f1,error=error,ploidy=ploidy))
+  min(apply(array(depth:(depth+10)),1,f1,error=error,ploidy=ploidy,prior=prior))
 }
